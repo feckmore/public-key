8639ac4fd4cd61c85d9d8dbd1ef5aa7f2603f119
David Schultz <dschultz@arthrex.com>
2018-02-22 18:28:06 -0600

    remove remnants of bygone package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 
-import { AngularFireModule } from "angularfire2";
-import { AngularFireDatabaseModule } from "angularfire2/database";
-import { AngularFireAuthModule } from "angularfire2/auth";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { ColorPickerModule } from "ngx-color-picker";
 
@@ -21,9 +18,6 @@ import { RestrictedInputDirective } from './directives/restricted-input.directiv
   imports: [
     FormsModule,
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule,
-    AngularFireAuthModule,
     NgbModule.forRoot(),
     ColorPickerModule
   ],
